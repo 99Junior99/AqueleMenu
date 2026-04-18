a67991ca33bc79fc9eb208ccfc8d4b56947ac416
Junior <joseinadilsonjunior@gmail.com>
2026-04-18 12:02:52 -0300

    melhorando a sidebar e adicionando a lista de produtos

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,11 +6,7 @@ import food from "../../assets/food/vecteezy_hamburger-food-png-generative-ai_27
 const Hero = () => {
     return (
         <div class='hero'>
-
-
             <img src={food} alt="" />
-
-
             <div className='hmsg'>
                 <h2>Aquele<span class="sub">Menu</span></h2>
                 <span>Um cardápio do seu jeito, feito especialmente para você.</span>
--- src/pages/pratos/Pratos.jsx
+++ src/pages/pratos/Pratos.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+//Componentes
 import Sidebar from '../../components/sidebar/Sidebar'
+import Lista from '../../components/lista/Lista'
+import './Pratos.css'
 
 const Pratos = () => {
     return (
-        <div>
+        <div className='pratos'>
             <Sidebar />
+            <Lista />
         </div>
     )
 }
